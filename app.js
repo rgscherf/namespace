91e952c7f5bc4d99b6b6c29d280cbefa25f3db89
rgscherf <rgscherf@gmail.com>
2015-09-17 22:40:33 -0700

    lots of cleanup

--- app.js
+++ app.js
@@ -1,44 +1,35 @@
-var animateApp = angular.module('animateApp', ['ngRoute', 'ngAnimate']);
+var animateApp = angular.module("animateApp", ["ngRoute", "ngAnimate"]);
 
-// ROUTING ===============================================
-// set our routing for this application
-// each route will pull in a different controller
 animateApp.config(function($routeProvider) {
-
     $routeProvider
-
-        // home page
-        .when('/', {
-            templateUrl: 'page-home.html',
-            controller: 'mainController'
+        .when("/", {
+            templateUrl: "page-home.html",
+            controller: "mainController"
         })
 
-        // about page
-        .when('/about', {
-            templateUrl: 'page-about.html',
-            controller: 'aboutController'
+        .when("/about", {
+            templateUrl: "page-about.html",
+            controller: "aboutController"
         })
 
-        // contact page
-        .when('/contact', {
-            templateUrl: 'page-contact.html',
-            controller: 'contactController'
+        .when("/contact", {
+            templateUrl: "page-contact.html",
+            controller: "contactController"
         });
 
 });
 
-// CONTROLLERS ============================================
-// home page controller
-animateApp.controller('mainController', function($scope) {
-    $scope.pageClass = 'page-home';
+animateApp.controller("mainController", function($scope) {
+    $scope.pageClass = "page-home";
+    $scope.navClass = "nav-home";
 });
 
-// about page controller
-animateApp.controller('aboutController', function($scope) {
-    $scope.pageClass = 'page-about';
+animateApp.controller("aboutController", function($scope) {
+    $scope.pageClass = "page-about";
+    $scope.navClass = "nav-about";
 });
 
-// contact page controller
-animateApp.controller('contactController', function($scope) {
-    $scope.pageClass = 'page-contact';
+animateApp.controller("contactController", function($scope) {
+    $scope.pageClass = "page-contact";
+    $scope.navClass = "nav-contact";
 });
